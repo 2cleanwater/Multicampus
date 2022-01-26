7815ff410fdbdd0fcc58d18005f41bd1f6b694cd
2cleanwater <32264455+2cleanwater@users.noreply.github.com>
2022-01-26 13:16:32 +0900

    Add files via upload

---
+++ algo220124/src/algo220124/LottoBall.java
@@ -0,0 +1,26 @@
+package algo220124;
+
+public class LottoBall {
+	private int ballNumber;
+
+	public LottoBall(int ballNumber){
+		this.ballNumber = ballNumber;
+	}
+	
+	public int getBallNumber() {
+		return ballNumber;
+	}
+	
+//		
+//	private void setBallNumber(int ballNumber) {
+//		this.ballNumber = ballNumber;
+//	}
+//	
+//		
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "나의 번호는 "+this.ballNumber+"입니다";
+	}
+	
+}
